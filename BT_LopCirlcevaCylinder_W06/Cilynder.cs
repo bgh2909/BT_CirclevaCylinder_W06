bd5262b1723ef6f4231945802e24b39564df190d
bgh2909 <bgh2909@gmail.com>
2024-03-03 00:02:24 +0700

    Add project files.

---
+++ BT_LopCirlcevaCylinder_W06/Cilynder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BT_LopCirlcevaCylinder_W06.Program;
+
+namespace BT_LopCirlcevaCylinder_W06
+{
+    internal class Cilynder
+    {
+        public class Cylinder : Circle
+        {
+            private double height;
+
+            public Cylinder()
+            {
+                height = 1.0;
+            }
+
+            public Cylinder(double radius, double height) : base(radius)
+            {
+                this.height = height;
+            }
+
+            public Cylinder(double radius, double height, string color) : base(radius, color)
+            {
+                this.height = height;
+            }
+
+            public double GetHeight()
+            {
+                return height;
+            }
+
+            public void SetHeight(double height)
+            {
+                this.height = height;
+            }
+
+            public double GetVolume()
+            {
+                return GetArea() * height;
+            }
+
+            public override string ToString()
+            {
+                return $"Cylinder[radius={GetRadius()}, height={height}, color={GetColor()}]";
+            }
+        }
+    }
+}
---
+++ BT_LopCirlcevaCylinder_W06/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BT_LopCirlcevaCylinder_W06.Cilynder;
+
+namespace BT_LopCirlcevaCylinder_W06
+{
+    internal class Program
+    {
+        public class Circle
+        {
+            private double radius;
+            private string color;
+
+            public Circle()
+            {
+                radius = 1.0;
+                color = "red";
+            }
+
+            public Circle(double radius)
+            {
+                this.radius = radius;
+                color = "red";
+            }
+
+            public Circle(double radius, string color)
+            {
+                this.radius = radius;
+                this.color = color;
+            }
+
+            public double GetRadius()
+            {
+                return radius;
+            }
+
+            public void SetRadius(double radius)
+            {
+                this.radius = radius;
+            }
+
+            public string GetColor()
+            {
+                return color;
+            }
+
+            public void SetColor(string color)
+            {
+                this.color = color;
+            }
+
+            public double GetArea()
+            {
+                return Math.PI * radius * radius;
+            }
+
+            public override string ToString()
+            {
+                return $"Circle[radius={radius}, color={color}]";
+            }
+        }
+            static void Main(string[] args)
+            {
+                Circle circle1 = new Circle();
+                Console.WriteLine(circle1);
+
+                Circle circle2 = new Circle(2.5);
+                Console.WriteLine(circle2);
+
+                Circle circle3 = new Circle(3.0, "blue");
+                Console.WriteLine(circle3);
+         
+
+                Cylinder cylinder1 = new Cylinder();
+                Console.WriteLine(cylinder1);
+
+                Cylinder cylinder2 = new Cylinder(2.0, 3.0);
+                Console.WriteLine(cylinder2);
+
+                Cylinder cylinder3 = new Cylinder(1.5, 4.0, "green");
+                Console.WriteLine(cylinder3);
+
+                Console.ReadKey();
+        }
+        }
+    }
+
